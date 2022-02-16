1e5335b279ddba4a04fe4382f81346a8356d92d6
ivan7cuw <ivan7cuw@gmail.com>
2022-02-16 22:01:48 +0700

    fix onSubmit

--- src/screens/Сreator/TradeHashCreate/index.tsx
+++ src/screens/Сreator/TradeHashCreate/index.tsx
@@ -78,7 +78,7 @@ export const TradeHashCreate: FC<Props> = ({ setTradeHash }) => {
        * If there is not enough money to make a transaction, the statement is not called
        */
 
-      if (!Web3.utils.toBN(allowance).gte(Web3.utils.toWei(values.senderAmount, 'ether'))) {
+fix      if (!Web3.utils.toBN(allowance).lt(Web3.utils.toWei(values.senderAmount, 'ether'))) {
         /**
          * is called to resolve the transaction
          */
