fecf63deba238a928044d12118d591ae9471c2d5
ivan7cuw <ivan7cuw@gmail.com>
2022-02-18 22:41:02 +0700

    del button

--- src/screens/SwitchRole/index.tsx
+++ src/screens/SwitchRole/index.tsx
@@ -6,53 +6,27 @@ import {
 import {
   Container,
 } from 'components';
-import { useLoadWeb3 } from 'hooks';
 import { ROUTES } from '../../constants';
 
 import styles from './styles.module.scss';
 
-export const SwitchRole: FC = () => {
-  const { methodsSwap } = useLoadWeb3();
-
-  async function onSubmit() {
-    // setIsLoading(true);
-
-    try {
-      const res = await methodsSwap?.transfers(
-        // eslint-disable-next-line @typescript-eslint/no-loss-of-precision
-        // `0x${28739489237489237489}`,
-        '0xb4f54665112c45f917a7e0db890f4a32de2526f9aa50934400da189a54c45d7c',
-      ).call();
-
-      console.log('res', res);
-      // setIsLoading(false);
-    } catch (e) {
-      // setIsLoading(false);
-      console.log(e);
-    }
-  }
-
-  return (
-    <Container
+export const SwitchRole: FC = () => (
+  <Container
       text=''
       title="Switch role"
     >
-      <div className={styles.container}>
-        <button onClick={onSubmit}>
-          Check contract
-        </button>
-        <Link to={ROUTES.creator.generating}>
-          Creator
-        </Link>
+    <div className={styles.container}>
+      <Link to={ROUTES.creator.generating}>
+        Creator
+      </Link>
 
-        <Link to={ROUTES.withdraw}>
-          Withdraw
-        </Link>
+      <Link to={ROUTES.withdraw}>
+        Withdraw
+      </Link>
 
-        <Link to={ROUTES.follower.generating}>
-          Follower
-        </Link>
-      </div>
-    </Container>
-  );
-};
+      <Link to={ROUTES.follower.generating}>
+        Follower
+      </Link>
+    </div>
+  </Container>
+);
